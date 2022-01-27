15e334d3e568ca1f3c0665721ae8ba6690c2b7e6
IMRANDIL <aliimranadil2@gmail.com>
2022-01-27 17:44:31 +0530

    yh

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-app.use('/', router)
+app.use('https://basic-chat-app001.herokuapp.com/', router)
 
 
 
 //post...
 
 
-app.post('/messages', (req, res) => {
+app.post('https://basic-chat-app001.herokuapp.com/messages', (req, res) => {
     const message = new Message(req.body);
     message.save((err) => {
         if (err) return sendStatus(500);
